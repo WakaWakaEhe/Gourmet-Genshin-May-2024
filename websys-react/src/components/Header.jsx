566fad0d7864f2ee06011eacfbfd45cf28b4faac
Yani <verdida.alyanna123@gmail.com>
2024-05-27 21:29:06 +0800

    modified header

--- websys-react/src/components/Header.jsx
+++ websys-react/src/components/Header.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import logo from '../img2/logo.png'
-import '../styles/style.css'
-import '../main.js'
+import React, { useState } from 'react';
+import logo from '../img2/logo.png';
+import '../styles/style.css';
+import '../main.js';
 
 function Head() {
+  // State to manage menu visibility
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
-        <a href="#" class="logo"><img src={logo} alt=""/></a>
-        <div class="bx bx-menu" id="menu-icon"></div>
-        <ul class="navbar">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#recipes">Recipes</a></li>
-            <li><a href="#regions">Regions</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-
+      <a href="#" className="logo"><img src={logo} alt=""/></a>
+      <div className="bx bx-menu" id="menu-icon" onClick={toggleMenu}></div>
+      {/* Conditional rendering of menu based on isMenuOpen state */}
+      <ul className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#recipes">Recipes</a></li>
+        <li><a href="#regions">Regions</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </header>
   );
 }
